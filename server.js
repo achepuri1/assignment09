49148c2a5e5549b920db4263ea5c8d986fbe0c3e
achepuri1 <157270299+achepuri1@users.noreply.github.com>
2024-02-26 13:07:56 -0500

    server.js

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const app = express();
 const axios = require('axios');
 const port = 3000;
 
-const myFunction = require('./my-function');
 
 
 app.use((req, res, next) => {
@@ -14,14 +13,29 @@ app.use((req, res, next) => {
 
 app.get('/say', async (req, res) => {
     try {
-        const keyword = req.query.keyword;
-        const functionResponse = await axios.get(`https://7iibd5ab3b.execute-api.us-east-2.amazonaws.com/dev/api-gateway/?name=Anjali&keyword=Hello`);
-        res.send(functionResponse.data);
-    } catch (error) {
-        console.error(error.message);
-        res.status(500).send('Internal Server Error');
+        const word = req.query.keyword;
+        console.log(word)
+        const functionResponse = await axios({
+            url: `https://7iibd5ab3b.execute-api.us-east-2.amazonaws.com/dev/api-gateway`,
+            method: 'get',
+    
+            data: {
+              keyword:word
+            }
+        })
+        //if(res.status == 200){
+            // test for status you want, etc
+           // console.log(res.status)
+        //}    
+        // Don't forget to return something   
+        res.send(functionResponse.data)
+    }
+    catch (err) {
+        console.error(err);
     }
 });
+
+
 
   app.listen(port, () => {
     console.log(`Server is listening at http://localhost:${port}`);
